d957d488f99847cf1f4b3420979682e758096529
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2026-05-21 11:06:06 -0300

    feat: Refactor App component structure and enhance UI elements

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,9 +1,13 @@
-import { Button } from "../../components/button";
+import { Button } from "../../components/ui/button";
+import { Header } from "../../components/popup-header";
+import { Toaster } from "../../components/ui/sonner";
+import { toast } from "sonner";
+import { Tabs, TabsContent } from "../../components/tabs";
 
 function App() {
-
   return (
-    <div>
+    <div className="p-4">
+      <Header />
       <h1>Hello World!!</h1>
       <h2>Hello World!!</h2>
       <p>Hello World!!</p>
@@ -13,6 +17,30 @@ function App() {
           Abrir Google
         </a>
       </Button>
+      <hr />
+      <Button variant="default" size="lg" onClick={() => toast.info("Hello World!!")}>
+          Abrir
+      </Button>
+      <hr />
+      <Tabs tabsTitles={["Overview", "Analytics", "Reports", "Settings"]} defaultValue="overview">
+        <TabsContent value="overview">
+          <p>Add any overview content you like here.</p>
+        </TabsContent>
+
+        <TabsContent value="analytics">
+          <p>Add any analytics content you like here.</p>
+        </TabsContent>
+
+        <TabsContent value="reports">
+          <p>Add any reports content you like here.</p>
+        </TabsContent>
+
+        <TabsContent value="settings">
+          <p>Add any settings content you like here.</p>
+        </TabsContent>
+      </Tabs>
+      <hr />
+      <Toaster />
     </div>
   );
 }
