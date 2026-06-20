da63e0015e841e4dac2f7754c88bbc4392c30d23
CarlosEduardo0077 <mendoncaeduardo652@gmail.com>
2026-06-20 17:04:13 -0300

    feat: Update Stripe checkout URL and add redirect timeout in handleUpgrade function

--- components/paywall-modal/handle-upgrade.tsx
+++ components/paywall-modal/handle-upgrade.tsx
@@ -3,9 +3,9 @@ import * as React from "react"
 export function handleUpgrade(setIsRedirecting: React.Dispatch<React.SetStateAction<boolean>>) {
     setIsRedirecting(true)
     
-    const STRIPE_CHECKOUT_URL = "https://buy.stripe.com/seu_link_aqui"
+    const STRIPE_CHECKOUT_URL = "https://buy.stripe.com/test_3cI14f8ef0izgribRK8bS00"
     
     window.open(STRIPE_CHECKOUT_URL, "_blank")
     
-    setIsRedirecting(false)
+    setTimeout(() => setIsRedirecting(false), 3000)
   }
